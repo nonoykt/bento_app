fb7d3faf23621e094c8bbf4a3a77de5a4f4d531a
hrkyjp <nonoykt@gmail.com>
2020-04-05 13:05:28 +0900

    Add validation to Bento model

--- app/models/bento.rb
+++ app/models/bento.rb
@@ -1,4 +1,4 @@
-class Bento <
+class Bento < ApplicationRecord
   before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
@@ -7,5 +7,5 @@ class Bento <
 
   def set_nameless_name
     self.name = '名前なし' if name.blank?
-  end  
+  end
 end
